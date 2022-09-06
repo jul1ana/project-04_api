be10f046587e59ac8db766b0e2a78f66ff00e3cd
jul1ana <julianaadt97@gmail.com>
2022-09-06 14:33:24 -0300

    Inicializando e criando o package.json junto com o server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const port = 3000;
+
+const rotas = {
+    "/api/v1": "Inicial",
+    "/api/v1/user": "Entrei em User"
+}
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end(rotas[req.url]);
+})
+
+server.listen(port, () => {
+    console.log(`Servidor escutando em http://localhost:${port}`);
+})
