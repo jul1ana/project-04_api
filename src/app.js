da15a2ce32bbc2eede755c22cb8eed0ed22f6093
jul1ana <julianaadt97@gmail.com>
2022-09-06 15:34:26 -0300

    Criando a interação dos métodos http com o postman

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const users = [
     {id: 1, "name": "Juliana"},
     {id: 2, "name": "Otávio"}
@@ -13,6 +15,11 @@ app.get("/api/v1", (req, res) => {
 
 app.get("/api/v1/user", (req, res) => {
     res.status(200).json(users);
+})
+
+app.post("/api/v1/user", (req, res) => {
+    users.push(req.body);
+    res.status(201).send("Usuário cadastrado com sucesso!");
 })
 
 export default app 
