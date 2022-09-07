421ec4a514e8b17bcc9161faf3e570639ab5204f
jul1ana <julianaadt97@gmail.com>
2022-09-07 14:36:33 -0300

    Adicionando mais propriedades no schema

--- src/models/User.js
+++ src/models/User.js
@@ -4,9 +4,17 @@ const userSchema = new mongoose.Schema(
     {
         id: {type: String},
         name: {type: String, required: true},
+        cpf: {type: String, required: true},
+        birthDate: {type: String, required: true},
+        email: {type: String, required: true},
+        password: {type: String, required: true},
+        address: {type: String, required: true},
+        number: {type: String, required: true},
+        complement: {type: String, required: true},
         city: {type: String, required: true},
+        state: {type: String, required: true},
         country: {type: String, required: true},
-        number: {type: Number}
+        zipCode: {type: String, required: true} 
     }
 );
 
