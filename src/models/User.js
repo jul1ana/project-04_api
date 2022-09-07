91d35ea433d9d6d332a2f7bef5d09e7afada9a92
jul1ana <julianaadt97@gmail.com>
2022-09-07 15:41:05 -0300

    Adicionando validação da senha

--- src/models/User.js
+++ src/models/User.js
@@ -4,14 +4,18 @@ function validatorCpf(cpf) {
     return cpf.length > 10 && cpf.length < 12;
 }
 
+function validatorSenha(senha) {
+    return senha.length > 5;
+}
+
 const userSchema = new mongoose.Schema(
     {
         id: {type: String},
         name: {type: String, required: true},
-        cpf: {type: String, validate: [validatorCpf, "este campo deve conter 11 caracteres."] , required: true},
+        cpf: {type: String, validate: [validatorCpf, "este campo deve conter 11 caracteres."], required: true},
         birthDate: {type: String, required: true},
         email: {type: String, required: true},
-        password: {type: String, required: true},
+        password: {type: String, validate: [validatorSenha, "este campo deve conter no mínimo 6 dígitos."], required: true},
         address: {type: String, required: true},
         number: {type: String, required: true},
         complement: {type: String, required: true},
